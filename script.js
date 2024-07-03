28cf9d642090d6264b65a9f40f3d3b6cdaa332e4
PureLeaf <volteczx8@gmail.com>
2024-07-03 10:32:50 -0400

    Modif J1900 done

---
+++ script.js
@@ -0,0 +1,9 @@
+// Generate a random number between 1 and 10
+function generateRandomNumber() {
+    return Math.floor(Math.random() * 10) + 1;
+  }
+  
+  // Usage example
+  let randomNumber = generateRandomNumber();
+  console.log("Random number generated:", randomNumber);
+  
